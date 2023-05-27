16e780fca6cf77933ac9534197d30fc52799672c
amosovandrey <rivercity.dp@gmail.com>
2023-05-27 17:47:15 +0300

    task 02 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,6 @@
-// Описан в документации
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.min.js';
-// Дополнительный импорт стилей
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
@@ -14,52 +10,18 @@ function createGalleryMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
 </li>`;
     })
     .join('');
 }
 
-function onItemClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const imageUrl = e.target.dataset.source;
-
-  modalOpen(imageUrl);
-}
-
-function modalOpen(imageUrl) {
-  const instance = basicLightbox.create(
-    `
-<img src="${imageUrl}">
-`,
-    {
-      onShow: () => {
-        window.addEventListener('keydown', modalClose);
-      },
-      onClose: () => {
-        window.removeEventListener('keydown', modalClose);
-      },
-    }
-  );
-
-  function modalClose(e) {
-    if (e.key === 'Escape') {
-      instance.close();
-    }
-  }
-
-  instance.show();
-}
-
-galleryEl.addEventListener('click', onItemClick);
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(e) {
+  const currentTime = e.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
